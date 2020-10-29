7e97d251cd5fce0e03949f81b004a204da56f4ec
DRLee98 <64233118+DRLee98@users.noreply.github.com>
2020-10-29 20:59:58 +0900

    Basic Styles
    기본 스타일 작성

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,14 +8,18 @@ import {
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Header from "Components/Header";
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" component={TV} />
-      <Route path="/search" component={Search} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
